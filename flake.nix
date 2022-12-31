059acb58ad75d631e123e5dbaecb2f2a836ad477
Max Veytsman <maxim@ontoillogical.com>
2022-12-30 22:46:11 -0500

    Format

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
     utils.url = "github:numtide/flake-utils";
     nixgl.url = "github:guibou/nixGL";
     nixelixir.url = "github:mveytsman/nix-elixir";
-
   };
 
   outputs = { self, nixpkgs, utils, nixelixir, nixgl }:
@@ -22,27 +21,26 @@
           beam.packages.erlangR25.elixir_1_14
           cudaPackages_11_8.cudatoolkit
           cudaPackages_11_8.libcublas
-cudaPackages_11_8.cudnn
+          cudaPackages_11_8.cudnn
           livebook
-        pkgs.nixgl.auto.nixGLNvidia
-];
+          pkgs.nixgl.auto.nixGLNvidia
+        ];
 
         libPath = lib.makeLibraryPath [
           cudaPackages_11_8.cudatoolkit
           cudaPackages_11_8.libcublas
-	cudaPackages_11_8.cudnn
+          cudaPackages_11_8.cudnn
         ];
 
       in {
         devShell = with pkgs;
           mkShell {
             buildInputs = basePackages;
- LD_LIBRARY_PATH = "${libPath}";
+            LD_LIBRARY_PATH = "${libPath}";
             shellHook = ''
-		export CUDA_DIR=${cudaPackages_11_8.cudatoolkit}
-		export XLA_FLAGS="--xla_gpu_cuda_data_dir=${cudaPackages_11_8.cudatoolkit}"
-	'';
-
+              export CUDA_DIR=${cudaPackages_11_8.cudatoolkit}
+              export XLA_FLAGS="--xla_gpu_cuda_data_dir=${cudaPackages_11_8.cudatoolkit}"
+            '';
           };
 
       });
